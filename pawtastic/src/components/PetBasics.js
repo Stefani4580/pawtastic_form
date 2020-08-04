01d5f924721d68a799e09e05a0ca9b7bf998ff12
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-08-04 15:15:38 -0400

    Added radio buttons

--- pawtastic/src/components/PetBasics.js
+++ pawtastic/src/components/PetBasics.js
@@ -26,9 +26,30 @@ export default class PetBasics extends Component {
                             placeholder="breed"
                             value={this.state.breed}
                     />
-                    <input type="checkbox" id="toggle" className="gender" />
-                    <label for="toggle" class="switch"></label>                    
-
+                    <input type="checkbox" id="gender" className="slider" />
+                    <label for="gender" class="switch"></label>                    
+                    <input type="checkbox" id="spayNeuter" className="slider" />
+                    <label for="spayNeuter" class="switch"></label>  
+                    <div className="weight">
+                        <div class="col-xl-10 pb-5">
+                            <input class="checkbox-budget" type="radio" name="budget" id="budget-1" checked />
+                            <label class="for-checkbox-budget" for="budget-1">
+                                <span data-hover="500$">500$</span>
+                            </label>
+                            <input class="checkbox-budget" type="radio" name="budget" id="budget-2" />
+                            <label class="for-checkbox-budget" for="budget-2">                          
+                                <span data-hover="1000$">1000$</span>
+                            </label>
+                            <input class="checkbox-budget" type="radio" name="budget" id="budget-3" />
+                            <label class="for-checkbox-budget" for="budget-3">                          
+                                <span data-hover="1500$">1500$</span>
+                            </label>
+                            <input class="checkbox-budget" type="radio" name="budget" id="budget-4" />
+                            <label class="for-checkbox-budget" for="budget-4">                          
+                                <span data-hover="2000$">2000$</span>
+                            </label>
+                        </div>   
+                    </div>
                 </div>
             </div>
         )
